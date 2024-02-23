4a12265fcc6692cc5876900d3bfdda7c005566c3
cyrilrommens <cyrilrommens@gmail.com>
2024-02-23 16:03:41 +0100

    commit

--- background_functions.py
+++ background_functions.py
@@ -2,19 +2,14 @@
 
 # Import necessary libraries
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
 import networkx as nx
-import seaborn as sns
-from itertools import combinations
-from scipy.spatial.distance import cdist
-import gudhi as gd
 
 # Define shannon entropy function
 def shannon_entropy(probabilities):
     # Remove any zero probabilities to avoid log(0) issues
     probabilities = probabilities[probabilities != 0]
-    return -np.sum(probabilities * np.log(probabilities))
+    return -np.sum(probabilities * np.log2(probabilities))
 
 # Define internal energy function
 def energy_function(p, A):
@@ -84,7 +79,6 @@ def free_energy(matrix, beta):
     # Set initial values
     list_size = len(matrix)
     num_iterations_energy = 1000
-    num_iterations_entropy = 100
     initial_probabilities = generate_probability_list(list_size)
 
     # Minimum internal energy with simulated annealing
@@ -98,6 +92,7 @@ def free_energy(matrix, beta):
 
     # Maximum shannon entropy with simulated annealing
     ####### To approximate the maximum entropy using Simulated Annealing unhash the line below and hash the uniform entropy method ######
+    #num_iterations_entropy = 100
     #max_entropy = simulated_annealing_entropy(initial_probabilities, num_iterations_entropy)
     
     return min_energy[-1]-beta*max_entropy[-1]
@@ -121,27 +116,6 @@ def generate_inverse_connectivity_matrix(clique_complex):
     inverse_connectivity_matrix = np.linalg.inv(matrix)
 
     return inverse_connectivity_matrix
-
-def file_to_matrix(file_name):
-    # Import the data of one subject
-    df = pd.read_csv(file_name,header=None)
-
-    # Extract the values from the DataFrame
-    data_str = df.iloc[:, 0].values
-
-    # Initialize an empty list to store parsed rows
-    parsed_data = []
-
-    # Iterate through each row and parse the space-separated values
-    for row_str in data_str:
-        # Split the string into a list of values
-        row_values = [float(val) for val in row_str.split()]
-        parsed_data.append(row_values)
-
-    # Convert the list of lists into a numpy array
-    M = np.array(parsed_data)
-
-    return M
 
 def simulated_annealing(test_matrix, num_runs, num_iterations_energy, num_iterations_entropy):
     # Example for generating energy and entropy data from a given matrix
@@ -170,60 +144,6 @@ def simulated_annealing(test_matrix, num_runs, num_iterations_energy, num_iterat
     energy_history_averaged = np.mean(energy_history, axis=0)
     return entropy_history, entropy_history_averaged, energy_history, energy_history_averaged
 
-def plot_SA_data(entropy_history, entropy_history_averaged, energy_history, energy_history_averaged):
-    # Plotting the optimisation side by side
-    plt.figure(figsize=(10, 5))
-
-    # Plot the energy/entropy evolution against the number of Simulated Annealing iterations
-
-    plt.subplot(1, 2, 1)
-    for i in range(0, len(energy_history)):
-        plt.plot(energy_history[i], color='grey', linestyle='--', linewidth=1)
-    if energy_history_averaged is not None:
-        plt.plot(energy_history_averaged, color='red', linestyle='-', linewidth=1, label = 'Average S for optimised p')
-        plt.legend()
-    plt.title('Minimum Energy U(p) (Simulated Annealing)')
-    plt.xlabel('Iterations')
-    plt.ylabel('Energy U(p)')
-    plt.grid(True)
-
-    plt.subplot(1, 2, 2)
-    for i in range(0, len(entropy_history)):
-        plt.plot(entropy_history[i], color='grey', linestyle='--', linewidth=1)
-    if entropy_history_averaged is not None:
-        plt.plot(entropy_history_averaged, color='red', linestyle='-', linewidth=1, label = 'Average S for optimised p')
-        plt.legend()
-    plt.title('Maximum Entropy S(p) (Simulated Annealing)')
-    plt.xlabel('Iterations')
-    plt.ylabel('Entropy S(p)')
-    plt.grid(True)
-
-    plt.tight_layout()  # Adjust layout for better spacing
-    plt.show()
-
-def generate_symmetric_binary_matrix(n):
-    # Generate a random binary matrix
-    random_matrix = np.random.randint(2, size=(n, n))
-
-    # Make the matrix symmetric by taking the upper triangular part and setting it equal to its transpose
-    symmetric_matrix = np.triu(random_matrix, 1) + np.triu(random_matrix, 1).T + np.eye(n)
-
-    return symmetric_matrix
-
-# Define function to draw a visual graph from the connection matrix
-def plot_graph(correlation_matrix, threshold):
-    n = correlation_matrix.shape[0]
-    G = nx.Graph()
-    
-    for i in range(n):
-        for j in range(i + 1, n):
-            if abs(correlation_matrix[i, j]) > threshold:
-                G.add_edge(i, j)
-
-    pos = nx.spring_layout(G)  # You can use a different layout if needed
-    nx.draw(G, pos, with_labels=True, font_weight='bold', node_size=700)
-    plt.show()
-
 # Define a function to obtain a list the simplexes present in the simplicial complex, by counting the complete subgraphs in the connection matrix
 def build_clique_complex(correlation_matrix, threshold, max_clique_size):
     n = correlation_matrix.shape[0]
@@ -249,20 +169,6 @@ def build_clique_complex(correlation_matrix, threshold, max_clique_size):
 
     return clique_complex
 
-def cliques_gudhi_matrix_comb(distance_matrix,f,d=2):
-  
-    
-    rips_complex = gd.RipsComplex(distance_matrix=distance_matrix, max_edge_length=f)
-    Rips_simplex_tree_sample = rips_complex.create_simplex_tree(max_dimension = d)
-    rips_generator = Rips_simplex_tree_sample.get_filtration()
-    
-    for i in rips_generator:
-        c,w=i
-        if w>0.:
-            yield c
-    
-    #return (i[0] for i in rips_generator if i[1]>0. )
-
 def compute_euler(Mat,cutoff,max_dim):
     eu=len(Mat)
     
@@ -275,42 +181,3 @@ def compute_euler(Mat,cutoff,max_dim):
     clique_complex = list(C)
 
     return [eu, clique_complex]
-
-def shannon_entropy(probabilities):
-    """Calculate the Shannon entropy of a probability distribution."""
-    entropy = -np.sum(probabilities * np.log2(probabilities))
-    return entropy
-
-def compute_shannon_entropy(M,cutoff,max_dim):
-    
-    epsilon=1e-20
-    
-    N={d:{} for d in range(1,max_dim+1)}
-    
-    G=nx.Graph(M)
-    Neigh={i:set(G.neighbors(i)) for i in G.nodes()}
-    
-    C=cliques_gudhi_matrix_comb(M, cutoff, max_dim)
-    eu=len(M)
-    for c in C:
-        
-        d=len(c)-1
-        eu+=(-1)**d
-        #print(d)
-        boundary=[b for b in combinations(c,d)]
-        n=sum([len(set.intersection(*[Neigh[j] for j in l])) for l in boundary])-1
-        try:
-            N[d][n]+=n
-        except:
-            N[d][n]=0
-            N[d][n]+=n
-            
-  
-    for d in N.keys():
-        norm=sum(N[d].values())
-        N[d]=[N[d][i]/norm for i in N[d].keys() if N[d][i]>=epsilon]
-     
-    E={d:shannon_entropy(N[d]) for d in N.keys()}
-    E["euler"]=eu
-     
-    return E
